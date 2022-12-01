c57d8d423a299fdecfcc1324f9f98fd8132aee0a
Geert Litjens <gjslitjens@gmail.com>
2022-12-01 18:52:55 +0100

    Solved Day 1 and setup environment

---
+++ src/advent_of_code_2022/days/day01/solution.py
@@ -0,0 +1,38 @@
+"""
+Pretty straightforward first day, with essentially mainly data parsing and summing / maxing
+"""
+
+from advent_of_code_2022.utils import Solution
+from typing import Any
+import numpy as np
+
+
+class DaySolution(Solution):
+    def __init__(self, day: int = 1, year: int = 2022) -> None:
+        super().__init__(day, year)
+
+    def _parse_data(self, input_data: str) -> Any:
+        """
+        Simply convert the numbers to ints and making a separate list for each elf
+        """
+        kcal_per_elf = []
+        kcal_cur_elf = []
+        for line in input_data.splitlines():
+            if line:
+                kcal_cur_elf.append(int(line))
+            else:
+                kcal_per_elf.append(kcal_cur_elf)
+                kcal_cur_elf = []
+        kcal_per_elf.append(kcal_cur_elf)
+        return kcal_per_elf
+
+    def _solve_part1(self, parsed_data: Any) -> Any:
+        """
+        Simply iterate over all elves and return the max
+        """
+        return np.max([np.sum(kcal_elf) for kcal_elf in parsed_data])
+
+    def _solve_part2(self, parsed_data: Any) -> Any:
+        """ """
+        sum_kcals_per_elf = sorted([np.sum(kcal_elf) for kcal_elf in parsed_data])
+        return np.sum(sum_kcals_per_elf[-3:])
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -1,21 +1,35 @@
 import pytest
-from day<DAY_NUMBER>.solution import DaySolution
+from advent_of_code_2022.days.day01.solution import DaySolution
 
 
 @pytest.fixture
 def day_testdata():
-    return ""
+    return """\
+1000
+2000
+3000
 
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000\
+"""
 
 def test_part1(day_testdata):
     sol = DaySolution()
     parsed_data = sol._parse_data(day_testdata)
     result = sol._solve_part1(parsed_data)
-    assert result == <TEST_ANSWER_1>
+    assert result == 24000
 
 
 def test_part2(day_testdata):
     sol = DaySolution()
     parsed_data = sol._parse_data(day_testdata)
     result = sol._solve_part2(parsed_data)
-    assert result == <TEST_ANSWER_2>
+    assert result == 45000
