c1c8c20c0273a4ed4742759506144b9bd384b417
Geert Litjens <gjslitjens@gmail.com>
2022-12-16 18:10:08 +0100

    Finally finished Day 15

--- src/advent_of_code_2022/days/day15/solution.py
+++ src/advent_of_code_2022/days/day15/solution.py
@@ -1,5 +1,6 @@
 """
 """
+import itertools
 import re
 import sys
 
@@ -43,33 +44,84 @@ class DaySolution(Solution):
                 )
         return len(no_beacon_pos) - len(beacons_in_row)
 
+    def _rotate_boxes(
+        self: "DaySolution", parsed_data: AoCData
+    ) -> list[list[list[list[int]]]]:
+        # First rotate all sensors 45 degrees to obtain square search spaces
+        boxes = []
+        for sen_bea in parsed_data:
+            sen, bea = sen_bea
+            dist = abs(sen[0] - bea[0]) + abs(sen[1] - bea[1])
+            corners = [
+                [sen[0] + dist, sen[1]],
+                [sen[0], sen[1] - dist],
+                [sen[0] - dist, sen[1]],
+                [sen[0], sen[1] + dist],
+            ]
+            rot_corners = [
+                [corners[0][0] - corners[0][1], corners[0][0] + corners[0][1]],
+                [corners[1][0] - corners[1][1], corners[1][0] + corners[1][1]],
+                [corners[2][0] - corners[2][1], corners[2][0] + corners[2][1]],
+                [corners[3][0] - corners[3][1], corners[3][0] + corners[3][1]],
+            ]
+            boxes.append(rot_corners)
+        return boxes
+
+    def _get_space_coords(
+        self: "DaySolution", boxes: list[list[list[list[int]]]]
+    ) -> tuple(set[int], set[int]):
+        # First find all columns that are exactly between two boxes
+        xs = set()
+        for box1, box2 in itertools.combinations(boxes, 2):
+            if abs(box1[2][0] - box2[1][0]) == 2:
+                xs.add(box1[2][0] - 1)
+            elif abs(box2[2][0] - box1[1][0]) == 2:
+                xs.add(box2[2][0] - 1)
+
+        ys = set()
+        for box1, box2 in itertools.combinations(boxes, 2):
+            if abs(box1[3][1] - box2[1][1]) == 2:
+                ys.add(box1[3][1] + 1)
+            elif abs(box2[3][1] - box1[1][1]) == 2:
+                ys.add(box2[3][1] + 1)
+        return xs, ys
+
+    def _check_box_coverage(
+        self: "DaySolution", x: int, y: int, boxes: list[list[list[list[int]]]]
+    ) -> list[int]:
+        cov_sides = [0, 0, 0, 0]
+        for box in boxes:
+            if box[0][0] == x - 1 and (box[1][1] <= y <= box[0][1]):
+                cov_sides[0] = 1
+                continue
+            elif box[2][0] == x + 1 and (box[1][1] <= y <= box[0][1]):
+                cov_sides[1] = 1
+                continue
+            elif box[0][1] == y - 1 and (box[2][0] <= x <= box[1][0]):
+                cov_sides[2] = 1
+                continue
+            elif box[2][1] == y + 1 and (box[2][0] <= x <= box[1][0]):
+                cov_sides[3] = 1
+                continue
+        return cov_sides
+
     def _solve_part2(self: "DaySolution", parsed_data: AoCData) -> AoCData:
         """ """
-        for row in range(4_000_001):
-            no_beacon_pos = set()
-            beacons_in_row = set()
-            for sen_bea in parsed_data:
-                if sen_bea[1][1] == row:
-                    beacons_in_row.add((sen_bea[1][0], sen_bea[1][1]))
-                dist = abs(sen_bea[0][0] - sen_bea[1][0]) + abs(
-                    sen_bea[0][1] - sen_bea[1][1]
-                )
-                if abs(sen_bea[0][1] - row) >= dist:
-                    continue
-                else:
-                    overlap = dist - abs(sen_bea[0][1] - row)
-                    no_beacon_pos.update(
-                        list(
-                            range(
-                                max(0, sen_bea[0][0] - overlap),
-                                min(4_000_000, sen_bea[0][0] + overlap + 1),
-                            )
-                        )
-                    )
-            if row % 100 == 0:
-                print(row)
-                print(len(no_beacon_pos))
-            if len(no_beacon_pos) == 3_999_999:
-                return (
-                    4_000_000 * no_beacon_pos.difference(list(range(4_000_001))) + row
-                )
+        if "pytest" in sys.modules:
+            search_range = 20
+        else:
+            search_range = 4_000_000
+
+        boxes = self._rotate_boxes(parsed_data)
+        xs, ys = self._get_space_coords(boxes)
+
+        for y in ys:
+            if 0 <= y <= 2 * search_range:
+                for x in xs:
+                    if -search_range <= x <= search_range:
+                        cov_sides = self._check_box_coverage(x, y, boxes)
+                        if sum(cov_sides) == 4:  # Found our point
+                            rot_x = (x + y) // 2
+                            rot_y = (y - x) // 2
+                            return 4000000 * rot_x + rot_y
+        return 1
--- tests/test_day15.py
+++ tests/test_day15.py
@@ -34,4 +34,4 @@ def test_part2(day_testdata: str) -> None:
     sol = DaySolution()
     parsed_data = sol._parse_data(day_testdata)
     result = sol._solve_part2(parsed_data)
-    assert result == 26
+    assert result == 56000011
