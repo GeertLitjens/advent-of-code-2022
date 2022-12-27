e977c1091766835879753afae41fa4137d974c97
Geert Litjens <gjslitjens@gmail.com>
2022-12-27 22:27:18 +0100

    Solved Day 18

--- src/advent_of_code_2022/days/day18/solution.py
+++ src/advent_of_code_2022/days/day18/solution.py
@@ -1,6 +1,9 @@
 """
 """
 
+import numpy as np
+from skimage.segmentation import flood_fill
+
 from advent_of_code_2022.utils import AoCData, Solution
 
 
@@ -10,12 +13,56 @@ class DaySolution(Solution):
 
     def _parse_data(self: "DaySolution", input_data: str) -> AoCData:
         """ """
-        return input_data
+        max_x, max_y, max_z = 0, 0, 0
+        poss = []
+        for line in input_data.splitlines():
+            poss.append([int(x) for x in line.split(",")])
+            if (x := poss[-1][0]) > max_x:
+                max_x = x
+            if (y := poss[-1][1]) > max_y:
+                max_y = y
+            if (z := poss[-1][2]) > max_z:
+                max_z = z
+        field = np.zeros((max_x + 3, max_y + 3, max_z + 3), dtype="byte")
+        for pos in poss:
+            field[pos[0] + 1, pos[1] + 1, pos[2] + 1] = 1
+        return field
 
     def _solve_part1(self: "DaySolution", parsed_data: AoCData) -> AoCData:
         """ """
-        return 1
+        open_sides = 0
+        for z in range(1, parsed_data.shape[2] - 1):
+            for y in range(1, parsed_data.shape[1] - 1):
+                for x in range(1, parsed_data.shape[0] - 1):
+                    if parsed_data[x, y, z] == 1:
+                        open_sides += (
+                            6
+                            - parsed_data[x - 1, y, z]
+                            - parsed_data[x + 1, y, z]
+                            - parsed_data[x, y - 1, z]
+                            - parsed_data[x, y + 1, z]
+                            - parsed_data[x, y, z - 1]
+                            - parsed_data[x, y, z + 1]
+                        )
+        return open_sides
 
     def _solve_part2(self: "DaySolution", parsed_data: AoCData) -> AoCData:
         """ """
-        return 1
+        filled_space = parsed_data + (
+            np.abs(flood_fill(parsed_data, (0, 0, 0), connectivity=1, new_value=1) - 1)
+        )
+        open_sides = 0
+        for z in range(1, filled_space.shape[2] - 1):
+            for y in range(1, filled_space.shape[1] - 1):
+                for x in range(1, filled_space.shape[0] - 1):
+                    if filled_space[x, y, z] == 1:
+                        open_sides += (
+                            6
+                            - filled_space[x - 1, y, z]
+                            - filled_space[x + 1, y, z]
+                            - filled_space[x, y - 1, z]
+                            - filled_space[x, y + 1, z]
+                            - filled_space[x, y, z - 1]
+                            - filled_space[x, y, z + 1]
+                        )
+        return open_sides
--- tests/test_day18.py
+++ tests/test_day18.py
@@ -5,18 +5,32 @@ from advent_of_code_2022.days.day18.solution import DaySolution
 
 @pytest.fixture
 def day_testdata() -> str:
-    return ""
+    return """\
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5\
+"""
 
 
 def test_part1(day_testdata: str) -> None:
     sol = DaySolution()
     parsed_data = sol._parse_data(day_testdata)
     result = sol._solve_part1(parsed_data)
-    assert result == 1
+    assert result == 64
 
 
 def test_part2(day_testdata: str) -> None:
     sol = DaySolution()
     parsed_data = sol._parse_data(day_testdata)
     result = sol._solve_part2(parsed_data)
-    assert result == 1
+    assert result == 58
